b1166843af42493644814ecd913e826c1565ac94
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2022-04-10 12:09:31 +0600

    update app.js
    setup route and import components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
+import { BrowserRouter } from 'react-router-dom';
+import Category from './components/Category';
+import Pages from './pages/Pages';
+
 function App() {
     return (
         <div className="App">
-            <h1>hello</h1>
+            <BrowserRouter>
+                <Category />
+                <Pages />
+            </BrowserRouter>
         </div>
     );
 }
