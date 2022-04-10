4345455c305a75dade98ced402a8c931395ccc97
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2022-04-10 12:07:24 +0600

    Create Search.jsx

---
+++ src/components/Search.jsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+import { useState } from 'react'
+import {FaSearch}  from 'react-icons/fa'
+
+function Search() {
+  return (
+    <div>Search</div>
+  )
+}
+
+const FormStyle = styled.from
+export default Search
