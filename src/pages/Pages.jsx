0085c33303b40ecfd39b4e96c88ea79c177a167a
Shaikh khoaiz Ahmmed <khoaizahmmed@gmail.com>
2022-04-11 00:59:50 +0600

    ad new page

--- src/pages/Pages.jsx
+++ src/pages/Pages.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
+import Searched from '../components/Searched';
 import Cuisine from './Cuisine';
 import Home from './Home';
+import Recipe from './Recipe';
 
 function Pages() {
     return (
         <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/cuisine/:type" element={<Cuisine />} />
+            <Route path="/Searched/:search" element={<Searched />} />
+            <Route path="/Recipe/:name" element={<Recipe />} />
         </Routes>
     );
 }
